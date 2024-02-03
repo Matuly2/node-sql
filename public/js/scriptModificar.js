773f0653c0637dff02ab86880ebcea5b0c81b7dc
Your Name <you@example.com>
2024-02-03 21:05:04 +0100

    borrar campos

--- public/js/scriptModificar.js
+++ public/js/scriptModificar.js
@@ -1,5 +1,6 @@
 var cursos;
 $(document).ready(() => {
+    
     $.ajax({
         url: '/cursosInfo',
         type: 'GET',
@@ -35,10 +36,10 @@ function habilitarCampos() {
 function renderCursos(jsonData) {
     const cursoSelect = document.getElementById('curso');
     
-    // Limpiar desplegable antes de renderizar
+    
     cursoSelect.innerHTML = '<option value="" disabled selected>Elige un curso para modificar</option>';
 
-    // Renderizar opciones desde el JSON
+    
     jsonData.forEach(curso => {
         const option = document.createElement('option');
         option.value = curso.nombreCurso;
@@ -62,7 +63,7 @@ function modificar() {
         nivel: nivelSelect.value
     };
 
-    console.log(valoresFormulario); // Puedes mostrarlo en la consola o realizar otras operaciones
+    
     Swal.fire({
         title: '¿Estás seguro?',
         text: 'Esta acción modificará el curso y no se puede deshacer',
@@ -73,7 +74,7 @@ function modificar() {
         confirmButtonText: 'Sí, modificar',
         cancelButtonText: 'Cancelar',
     }).then((result) => {
-        // Si se confirma la modificación
+       
         if (result.isConfirmed) {
             
             $.ajax({
@@ -82,10 +83,12 @@ function modificar() {
                 dataType: 'json',
                 data: valoresFormulario, 
                 success: function(data) {
-                    // Lógica de éxito después de modificar
-                    Swal.fire('Modificado', 'El curso ha sido modificado', 'success').then(() => {
                         
+                    Swal.fire('Modificado', 'El curso ha sido modificado', 'success').then(() => {
+                        borrarCampos();
                         location.reload();
+                    
+                        
                     });
                 },
                 error: function(error) {
@@ -99,4 +102,10 @@ function modificar() {
     Swal.fire('Error', 'Rellene todos los campos.', 'error');
 }
 
+}
+function borrarCampos(){
+    document.getElementById('nombre').value="";
+    document.getElementById('lugar').value="";
+    document.getElementById('descripcion').value="";
+    document.getElementById('nivel').value="";
 }
